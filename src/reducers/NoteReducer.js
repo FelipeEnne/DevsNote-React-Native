fa733031f5525cffe8571c8d91f63c9938dead0c
FelipeEnne <felipeenne@gmail.com>
2022-05-10 21:59:43 -0300

    first commit

---
+++ src/reducers/NoteReducer.js
@@ -0,0 +1,13 @@
+import {act} from 'react-test-renderer';
+
+const initialState = {
+  list: [{title: 'Primeira Nota', body: 'Testando 123'}],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_NOTE':
+      break;
+  }
+  return state;
+};
